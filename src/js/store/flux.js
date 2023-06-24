abcb533c671a735d7afcaf29d6960ffd5a83438e
ValentinFrAr <116929850+ValentinFrAr@users.noreply.github.com>
2023-06-24 21:49:03 +0000

    new

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -3,67 +3,73 @@ const getState = ({ getStore, getActions, setStore }) => {
     store: {
       people: [],
       character: [],
-			planets: [],
-			planet: []
+      planets: [],
+      planet: [],
     },
     actions: {
       // Use getActions to call a function within a fuction
       getPeople: async () => {
-				const settings = {
-					method: "GET",
-					headers: { "Content-Type": "application/json" }
-				};
+        const settings = {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        };
 
-				const request = await fetch(`https://www.swapi.tech/api/people`, settings);
-				const json = await request.json();
-				const data = json;
-				setStore({ people: data.results });
-			},
+        const request = await fetch(
+          `https://www.swapi.tech/api/people`,
+          settings
+        );
+        const json = await request.json();
+        const data = json;
+        setStore({ people: data.results });
+      },
 
-			getCharacterDescription: async url => {
-				const store = getStore();
-				const settings = {
-					method: "GET",
-					headers: { "Content-Type": "application/json" }
-				};
+      getCharacterDescription: async (url) => {
+        const store = getStore();
+        const settings = {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        };
 
-				const request = await fetch(url, settings);
-				const json = await request.json();
-				const data = json;
-				setStore({ character: [...store.character, data.result.properties] });
-			},
+        const request = await fetch(url, settings);
+        const json = await request.json();
+        const data = json;
+        setStore({ character: [...store.character, data.result.properties] });
+      },
 
-			charDescription: url => {
-				getActions().getCharacterDescription(url);
-			},
-			getPlanet: async () => {
-				const settings = {
-					method: "GET",
-					headers: { "Content-Type": "application/json" }
-				};
+      charDescription: (url) => {
+        getActions().getCharacterDescription(url);
+      },
+      getPlanet: async () => {
+        const settings = {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        };
 
-				const request = await fetch(`https://www.swapi.tech/api/people`, settings);
-				const json = await request.json();
-				const data = json;
-				setStore({ planets: data.results });
-			},
+        const request = await fetch(
+          `https://www.swapi.tech/api/planets`,
+          settings
+        );
+        const json = await request.json();
+        const data = json;
+        setStore({ planets: data.results });
+      },
 
-			getPlanetDescription: async url => {
-				const store = getStore();
-				const settings = {
-					method: "GET",
-					headers: { "Content-Type": "application/json" }
-				};
+      getPlanetDescription: async (url) => {
+        const store = getStore();
+        const settings = {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        };
 
-				const request = await fetch(url, settings);
-				const json = await request.json();
-				const data = json;
-				setStore({ planet: [...store.planet, data.result.properties] });
-			},
+        const request = await fetch(url, settings);
+        const json = await request.json();
+        const data = json;
+        setStore({ planet: [...store.planet, data.result.properties] });
+      },
 
-			planetDescription: url => {
-				getActions().getPlanetDescription(url);
-			},
+      planetDescription: (url) => {
+        getActions().getPlanetDescription(url);
+      },
     },
   };
 };
