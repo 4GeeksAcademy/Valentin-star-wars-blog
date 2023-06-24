8535b4e8ed4dca42d7d8719f3c68dcc3fe359e0d
ValentinFrAr <116929850+ValentinFrAr@users.noreply.github.com>
2023-06-24 05:20:50 +0000

    new

--- src/js/component/CardsPeople.jsx
+++ src/js/component/CardsPeople.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useContext } from "react";
 import { Context } from "../store/appContext.js";
+import { Link } from "react-router-dom";
 
 const CardsPeople = (props) => {
   const { store, actions } = useContext(Context);
-  const charStore = store.character.filter(char => char.name == props.character.name);
-	useEffect(() => actions.charDescription(props.character.url), []);
+  const charStore = store.character.filter(
+    (char) => char.name == props.character.name
+  );
+  useEffect(() => actions.charDescription(props.character.url), []);
 
   return (
     <div className="cards">
       <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
+        <img
+          src="https://media.paperblog.fr/i/623/6232954/personnage-prefere-fans-star-wars-votez-maint-L-YCAQwN.jpeg"
+          className="card-img-top"
+          alt="..."
+        />
         <div className="card-body">
           <h5 className="card-title">{props.character.name}</h5>
           {charStore[0] ? (
@@ -21,7 +28,15 @@ const CardsPeople = (props) => {
           ) : (
             ""
           )}
-          <button className="btn btn-primary">Go somewhere</button>
+          <div className="d-flex justify-content-center">
+            <Link
+              to={"/single/" + props.character.uid}
+              className="more"
+              data={charStore}
+            >
+              <button className="btn btn-more">Learn More</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
